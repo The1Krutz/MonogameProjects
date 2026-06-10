2317bb05142fa11e779cd680c2628a1cfe367a51
Krutz <the1krutz@gmail.com>
2026-06-09 23:14:24 -0400

    Demo2 works now

--- Demo2/Game1.cs
+++ Demo2/Game1.cs
@@ -2,15 +2,19 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace Demo2 {
-  public class Game1 : Game {
+namespace Demo2
+{
+  public class Game1 : Game
+  {
     GraphicsDeviceManager graphics;
     SpriteBatch spriteBatch;
     Texture2D potatoTexture;
     Texture2D spriteTexture;
 
-    public Game1() {
-      graphics = new GraphicsDeviceManager(this) {
+    public Game1()
+    {
+      graphics = new GraphicsDeviceManager(this)
+      {
         PreferredBackBufferWidth = 500,
         PreferredBackBufferHeight = 500
       };
@@ -18,24 +22,28 @@ namespace Demo2 {
       IsMouseVisible = true;
     }
 
-    protected override void Initialize() {
+    protected override void Initialize()
+    {
       // TODO: Add your initialization logic here
 
       base.Initialize();
     }
 
-    protected override void LoadContent() {
+    protected override void LoadContent()
+    {
       spriteBatch = new SpriteBatch(GraphicsDevice);
       potatoTexture = Content.Load<Texture2D>("potato");
       spriteTexture = Content.Load<Texture2D>("sprite_haha");
     }
 
-    protected override void UnloadContent() {
+    protected override void UnloadContent()
+    {
       Content.Unload();
       base.UnloadContent();
     }
 
-    protected override void Update(GameTime gameTime) {
+    protected override void Update(GameTime gameTime)
+    {
       if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
         Exit();
 
@@ -44,13 +52,14 @@ namespace Demo2 {
       base.Update(gameTime);
     }
 
-    protected override void Draw(GameTime gameTime) {
+    protected override void Draw(GameTime gameTime)
+    {
       GraphicsDevice.Clear(Color.CornflowerBlue);
 
       spriteBatch.Begin(sortMode: SpriteSortMode.FrontToBack);
-      spriteBatch.Draw(spriteTexture, Vector2.Zero, layerDepth: 1.0f, color: Color.White);
+      spriteBatch.Draw(spriteTexture, Vector2.Zero, color: Color.White);
       spriteBatch.Draw(spriteTexture, new Vector2(100, 0), Color.White);
-      spriteBatch.Draw(potatoTexture, destinationRectangle: new Rectangle(0, 0, 500, 500), layerDepth: 0.0f, color: Color.White);
+      spriteBatch.Draw(potatoTexture, destinationRectangle: new Rectangle(0, 0, 500, 500), color: Color.White);
       spriteBatch.End();
 
       base.Draw(gameTime);
